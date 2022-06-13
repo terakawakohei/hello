c3ed69b0019e0eaff4c8fc389e60e5ad36d97cb9
Kohei Terakawa <odayakalife@teraka.local>
2022-06-13 14:10:50 +0900

    入力バリディーションを追加

--- NumbersGame.java
+++ NumbersGame.java
@@ -1,27 +1,38 @@
 public class NumbersGame {
 
+public int inputPositiveNumber(){
+		int inputNumber = KeyBoard.inputNumber();
+
+        while(inputNumber<0||inputNumber>100){
+        System.out.println("値が相応しくありません。もう一度、2桁の整数を入力してください");
+		inputNumber = KeyBoard.inputNumber();
+        }
+        return inputNumber;
+}
+
 public  void askNumber(){
         int target=24;
+        System.out.println("数当てゲームを始めます");
+        System.out.println("2桁の整数を入力してください :");
 
         //解答できる数
         int limit=5;
         while(limit>0){
-        System.out.println("予想した数を入力してください : ");
-		int inputNumber = KeyBoard.inputNumber();
+        int inputNumber=inputPositiveNumber();
         if(target==inputNumber){
-        System.out.println("正解！");
+        System.out.println("正解！ゲームクリアです");
         break;
         }else{
-        System.out.println("違います");
+        System.out.println("不正解...");
         int diff=target-inputNumber;
         if(diff>0){
-        System.out.println("設定値より小さいです");
+        System.out.println("→ 正解値より小さいです");
         }else {
-        System.out.println("設定値より大きいです");
+        System.out.println("→ 正解値より大きいです");
 
         }
         if(Math.abs(diff)>20){
-        System.out.println("20以上差があります");
+        System.out.println("→ 20以上差があります");
         }
         }
 
